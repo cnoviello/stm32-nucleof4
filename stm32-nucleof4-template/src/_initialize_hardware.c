6c79f75387abde3c4373f88eea9fb8fef5a119e1
Carmine Noviello <cnoviello@hotmail.com>
2015-01-11 10:47:03 +0100

    Initial import

---
+++ stm32-nucleof4-template/src/_initialize_hardware.c
@@ -0,0 +1,175 @@
+//
+// This file is part of the µOS++ III distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_cortex.h"
+
+// ----------------------------------------------------------------------------
+
+// The external clock frequency is specified as a preprocessor definition
+// passed to the compiler via a command line option (see the 'C/C++ General' ->
+// 'Paths and Symbols' -> the 'Symbols' tab, if you want to change it).
+// The value selected during project creation was HSE_VALUE=8000000.
+//
+// The code to set the clock is at the end.
+//
+// Note1: The default clock settings assume that the HSE_VALUE is a multiple
+// of 1MHz, and try to reach the maximum speed available for the
+// board. It does NOT guarantee that the required USB clock of 48MHz is
+// available. If you need this, please update the settings of PLL_M, PLL_N,
+// PLL_P, PLL_Q to match your needs.
+//
+// Note2: The external memory controllers are not enabled. If needed, you
+// have to define DATA_IN_ExtSRAM or DATA_IN_ExtSDRAM and to configure
+// the memory banks in system/src/cmsis/system_stm32f4xx.c to match your needs.
+
+// ----------------------------------------------------------------------------
+
+// Forward declarations.
+
+void
+__initialize_hardware(void);
+
+void
+configure_system_clock(void);
+
+// ----------------------------------------------------------------------------
+
+// This is the application hardware initialisation routine,
+// redefined to add more inits.
+//
+// Called early from _start(), right after data & bss init, before
+// constructors.
+//
+// After Reset the Cortex-M processor is in Thread mode,
+// priority is Privileged, and the Stack is set to Main.
+
+void
+__initialize_hardware(void)
+{
+  // Call the CSMSIS system initialisation routine.
+  SystemInit();
+
+#if defined (__VFP_FP__) && !defined (__SOFTFP__)
+
+  // Enable the Cortex-M4 FPU only when -mfloat-abi=hard.
+  // Code taken from Section 7.1, Cortex-M4 TRM (DDI0439C)
+
+  // Set bits 20-23 to enable CP10 and CP11 coprocessor
+  SCB->CPACR |= (0xF << 20);
+
+#endif // (__VFP_FP__) && !(__SOFTFP__)
+
+  // Initialise the HAL Library; it must be the first
+  // instruction to be executed in the main program.
+  HAL_Init();
+
+  // Warning: The HAL always initialises the system timer.
+  // For this to work, the default SysTick_Handler must not hang
+  // (see system/src/cortexm/exception_handlers.c)
+
+  // Unless explicitly enabled by the application, we prefer
+  // to keep the timer interrupts off.
+  HAL_SuspendTick();
+
+  // Enable HSE Oscillator and activate PLL with HSE as source
+  configure_system_clock();
+}
+
+#if 0
+
+// This is a sample SysTick handler, use it if you need HAL timings.
+void __attribute__ ((section(".after_vectors")))
+SysTick_Handler(void)
+{
+	HAL_IncTick();
+}
+
+#endif
+
+// ----------------------------------------------------------------------------
+
+/**
+ * @brief  System Clock Configuration
+ *         The system Clock is configured as follow :
+ *            System Clock source            = PLL (HSE)
+ *            SYSCLK(Hz)                     = 168000000
+ *            HCLK(Hz)                       = 168000000
+ *            AHB Prescaler                  = 1
+ *            APB1 Prescaler                 = 4
+ *            APB2 Prescaler                 = 2
+ *            HSE Frequency(Hz)              = HSE_VALUE
+ *            PLL_M                          = (HSE_VALUE/1000000u)
+ *            PLL_N                          = 336
+ *            PLL_P                          = 2
+ *            PLL_Q                          = 7
+ *            VDD(V)                         = 3.3
+ *            Main regulator output voltage  = Scale1 mode
+ *            Flash Latency(WS)              = 5
+ * @param  None
+ * @retval None
+ */
+void
+configure_system_clock(void)
+{
+#if defined(NUCLEO_F401)
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+	__PWR_CLK_ENABLE();
+
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 6;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 16;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+	RCC_OscInitStruct.PLL.PLLQ = 7;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+#elif defined(NUCLEO_F411)
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+	__PWR_CLK_ENABLE();
+
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 6;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 16;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+#else
+#error "Please, set your nucleo board type in project preferences!"
+#endif
+}
+
+// ----------------------------------------------------------------------------
---
+++ stm32-nucleof4-template/src/main.c
@@ -0,0 +1,46 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include "diag/Trace.h"
+
+// ----------------------------------------------------------------------------
+//
+// Standalone STM32F4 empty sample (trace via NONE).
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the NONE output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+int
+main(int argc, char* argv[])
+{
+  // At this stage the system clock should have already been configured
+  // at high speed.
+
+  // Infinite loop
+  while (1)
+    {
+       // Add your code here.
+    }
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
